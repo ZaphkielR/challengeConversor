a6e2de36064e9397a24bc9bd6c5c7e2bb1a1cb41
ZaphkielR <rm1423aa@gmail.com>
2024-07-13 07:50:32 -0400

    first commit

---
+++ src/main/java/com/challenge/ConversorDeMonedas/ConversorDeMonedasApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.ConversorDeMonedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDeMonedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorDeMonedasApplication.class, args);
+	}
+
+}
